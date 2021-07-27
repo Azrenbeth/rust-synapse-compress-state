328cbcc8e89196f0dafe9548276cd3d023a3c433
Azrenbeth <7782548+Azrenbeth@users.noreply.github.com>
2021-07-27 10:54:10 +0100

    Moved over auto_compressor integration tests

---
+++ compressor_integration_tests/tests/auto_compressor_state_saving_tests.rs
@@ -0,0 +1,30 @@
+use auto_compressor::{
+    state_saving::{
+        connect_to_database, create_tables_if_needed, read_room_compressor_state,
+        write_room_compressor_state,
+    },
+    LevelState,
+};
+use compressor_integration_tests::{DB_URL, empty_database};
+use serial_test::serial;
+
+
+#[test]
+#[serial(db)]
+fn write_then_read_state_gives_correct_results() {
+    let mut client = connect_to_database(DB_URL).unwrap();
+    create_tables_if_needed(&mut client).unwrap();
+    empty_database();
+
+    let room_id = "room1";
+    let written_info: Vec<LevelState> = vec![(3, 1, Some(6)), (3, 2, Some(6))];
+    let written_num = 53;
+    write_room_compressor_state(&mut client, room_id, &written_info, written_num).unwrap();
+
+    let (read_num, read_info) = read_room_compressor_state(&mut client, room_id)
+        .unwrap()
+        .unwrap();
+
+    assert_eq!(written_info, read_info);
+    assert_eq!(written_num, read_num);
+}
